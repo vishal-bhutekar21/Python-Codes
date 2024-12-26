f2a9adea63bb2b997f3aa0cc7e40140842b57731
Vishal-bhutekekar21 <vishalbhutekar33772@gmail.com>
2024-12-27 00:26:21 +0530

    day 1

---
+++ unit 1.1 stringFormating/inOneLine.py
@@ -0,0 +1,4 @@
+# if we do not want something to not print on new line we can make use of end by making it empty 
+
+print("Government College of Engineering ,Aurangabad",end="")
+print(",Chh Sambhajinagar ")

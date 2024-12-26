b1367b8ea2ded23579a557470023224ad0b27666
Vishal-bhutekekar21 <vishalbhutekar33772@gmail.com>
2024-12-27 00:18:18 +0530

    day 1

---
+++ unit 1.1 stringFormating/UsingOperator.py
@@ -0,0 +1,2 @@
+name,age,salary,alive="vishal ",21,39000,True
+print("My name is %s, My Age is %d ,My Salary is %d ,My livins status is %s "% (name,age,salary,alive))
---
+++ unit 1.1 stringFormating/usingFormarfSeperator.py
@@ -0,0 +1,2 @@
+name,age,salary,alive="vishal ",21,39000,True
+print("My name is : {} My age is : {} My salary is : {} My living status is : {}".format(name,age,salary,alive))

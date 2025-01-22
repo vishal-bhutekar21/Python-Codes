0d1d59abc78520eb232e6c670c4a273265a73c71
Vishal-bhutekekar21 <vishalbhutekar33772@gmail.com>
2025-01-22 16:41:16 +0530

    added swapping program wihtout using 3 var

---
+++ Unit 1 Basics/Swap2numbers.py
@@ -0,0 +1,30 @@
+a=100
+b=200
+
+print("the value before the swap of a is",a)
+print("the value before the swap of b is",b)
+
+# a=a+b
+# b=a-b
+# a=a-b
+
+# a=a^b
+# b=a^b
+# a=a^b
+
+
+a=a*b
+b=a/b
+a=a//b
+print("the value after the swap of a is",a)
+print("the value after the swap of b is",b)
+
+
+a1=10
+b1=20
+
+
+
+
+
+# Naresh it solution
